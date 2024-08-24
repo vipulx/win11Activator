0c73af9046acc0ce34d2dcd15e5d73ee45bc2ba6
Vipul Raj <155657964+Vipulraj0152@users.noreply.github.com>
2024-08-25 04:22:39 +0530

    Create Setup.bat

---
+++ Setup.bat
@@ -0,0 +1,47 @@
+@echo off
+rem Project Information Script
+rem Displays details about a project
+
+:: Clear the screen
+cls
+
+:: Set color to green on black
+color 0A
+
+:: Print an ASCII art logo
+echo.
+echo "   _________.__    .___  .___.__                   __                        __            "
+echo "  /   _____/|__| __| _/__| _/|  |__ _____ ________/  |________  ____   ____ |  | _____  ___"
+echo "  \_____  \ |  |/ __ |/ __ | |  |  \\__  \\_  __ \   __\_  __ \/  _ \_/ ___\|  |/ /\  \/  /"
+echo "  /        \|  / /_/ / /_/ | |   Y  \/ __ \|  | \/|  |  |  | \(  <_> )  \___|    <  >    < "
+echo " /_______  /|__\____ \____ | |___|  (____  /__|   |__|  |__|   \____/ \___  >__|_ \/__/\_ \"
+echo "         \/         \/    \/      \/     \/                               \/     \/      \/"
+
+:: Print separator line
+echo.
+echo "==============================================="
+
+:: Provide tool information
+echo.
+echo "         Siddharttrockx Windows 11 Updater"
+echo         
+echo "==============================================="
+echo.
+echo "Welcome to the Siddharttrockx Windows 11 Updater tool."
+echo "This tool will help you update your Windows 11 system easily."
+echo "Please follow the on-screen instructions to proceed."
+echo.
+echo "For more information, visit our GitHub page: https://github.com/Vipulraj0152/win11Activator"
+echo.
+echo "Editor Information:"
+echo "===================="
+echo "Name: Vipul Raj"
+echo "Username: vipulraj0152"
+echo "Version: 1.0.0.1"
+echo "Email: vipulraj0152@gmail.com"
+echo.
+echo "For further support, please contact us via GitHub or email."
+echo.
+
+:: Pause the script
+pause
